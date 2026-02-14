1405d8634fff2eecb6895ad2c5cbf710306e0bfe
Bruno Rael Vieira <brunorael59@gmail.com>
2026-02-13 22:41:20 -0300

    Js adicionado

---
+++ scripts.js
@@ -0,0 +1,50 @@
+
+const form = document.querySelector (".formulario-fale-conosco")
+const mascara = document.querySelector (".mascara-form")
+
+function mostrarForm(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderMascara(){
+    form.style.left = "-20%"
+    form.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*VARIÁVEIS
+É UMA ESPAÇO DE MEMÓRIA QUE VOCÊ RESERVA E PODE GUARADR O QUE QUISER LÁ DENTRO
+
+3 JEITOS DE CRIAR UMA VARIÁVEL
+-var -> NÃO USAR MAIS, FOI DESCONTINUADO, ANTIGO.
+-let -> VOCÊ PODE ALTERAR O VALOR
+-const -> ELE É CONSTANTE, NÃO TEM COMO MUDAR, VAI SER O MESMO DO INÍCIO AO FIM*/
+
+
+/*FUNÇÕES
+É UM TRECHO DE CÓDIGO QUE SÓ É EXECUTADO QUANDO É CHAMADO*/
+
+/*O JAVA PERMITE MANIPULAR A DOM, MAS O QUE É A DOM?
+É A NOSSA ÁRVORE DE ELEMENTOS, AONDE TEMOS A DIV, IMAGEM, BODY ENTRE OUTROS*/
+
+
+
+
+
+
+
+
+
